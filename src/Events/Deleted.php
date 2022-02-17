a9c49e032b901813fee2d1bbf08e9d9d12c8ef25
jabber <2898117012@qq.com>
2022-02-17 21:52:06 +0800

    fix: event

--- src/Events/Deleted.php
+++ src/Events/Deleted.php
@@ -2,6 +2,11 @@
 
 namespace Dealskoo\Image\Events;
 
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
 class Deleted extends Event
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 }
--- src/Events/Event.php
+++ src/Events/Event.php
@@ -3,9 +3,14 @@
 namespace Dealskoo\Image\Events;
 
 use Dealskoo\Image\Models\Image;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class Event
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public $image;
 
     public function __construct(Image $image)
